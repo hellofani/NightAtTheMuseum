30bfc646640c442a39ba4ab21eb190d4a85cc587
hellofani <hellofani@yahoo.com>
2018-03-03 15:31:15 +0530

    Textures Folder added

---
+++ Assets/Scripts/VideoPlay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlay : MonoBehaviour {
+
+	public Material playBtnMat;
+	public Material pauseBtnMat;
+	public Renderer screenRenderer;
+
+	private VideoPlayer videoPlayer;
+
+	void Awake(){
+		videoPlayer = GetComponent<VideoPlayer>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    public void PlayPause(){
+    	if (videoPlayer.isPlaying){
+    		videoPlayer.Pause();
+    		screenRenderer.material = playBtnMat;
+    	} else {
+    		videoPlayer.Play();
+    		screenRenderer.material = pauseBtnMat;
+    	}
+    }
+}
